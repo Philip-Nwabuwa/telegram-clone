88d5498d30e9912d60a1cee4e02b7e9db5bd9b04
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2024-02-26 21:19:18 +0000

    chore: added pages for auth pages.

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,60 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import RegisterAuthForm from "@/components/module/RegisterAuthForm";
+
+export const metadata: Metadata = {
+  title: "Reguster page",
+  description: "Register to chat with your friends",
+};
+
+export default function RegisterPage() {
+  return (
+    <>
+      <div className="container relative h-screen flex-col items-center justify-center grid">
+        <Link
+          href="/login"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "absolute right-4 top-4 md:right-8 md:top-8"
+          )}
+        >
+          Login
+        </Link>
+
+        <div>
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Create an account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email below to create your account
+              </p>
+            </div>
+            <RegisterAuthForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
