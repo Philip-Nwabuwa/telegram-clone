ef8ff15c058f51ebd8074e8176a2eb464e79aafd
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2024-03-01 17:11:31 +0000

    chore: added basic input form.

--- src/components/module/chatpage/Message.tsx
+++ src/components/module/chatpage/Message.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import { Mic, Paperclip, SendHorizontal, Smile } from "lucide-react";
+import { useState } from "react";
+
 interface MessageProps {
   messages: string[];
 }
 
 const Message = ({ messages }: MessageProps) => {
+  const [inputText, setInputText] = useState("");
   if (!messages || messages.length === 0) {
     return (
       <div>
@@ -11,10 +17,34 @@ const Message = ({ messages }: MessageProps) => {
     );
   }
   return (
-    <div>
-      {messages.map((message, index) => {
-        return <p key={index}>{message}</p>;
-      })}
+    <div className="scrollView flex flex-col gap-4 h-full items-center justify-end">
+      <div>
+        {messages.map((message, index) => {
+          return <p key={index}>{message}</p>;
+        })}
+      </div>
+      <div className="flex items-center justify-center gap-2 max-w-[700px] w-full">
+        <div className="w-full flex space-x-4 bg-gray-950 rounded-2xl px-5 py-4">
+          <Smile />
+
+          <input
+            className="flex-1 bg-transparent outline-none"
+            type="text"
+            placeholder="Type a message"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
+
+          <Paperclip />
+        </div>
+        <div className="flex items-center justify-center bg-red-500 rounded-full p-4">
+          {inputText ? (
+            <SendHorizontal onClick={() => console.log("Send clicked")} />
+          ) : (
+            <Mic onClick={() => console.log("Mic clicked")} />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
